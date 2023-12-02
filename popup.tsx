352dca547b5b287fca3d9e5bea3a93e4ed4e5c83
KLAKALU <player4797462226@gmail.com>
2023-12-02 16:04:43 +0900

    add: view extansion popup

--- popup.tsx
+++ popup.tsx
@@ -1,27 +1,56 @@
 import { useState } from "react"
+import { SecureStorage } from "@plasmohq/storage/secure"
+
+import "./style.css"
+
+function loginComponent( {showForm, setShowForm, data, setData} ) {
+  return (
+    <div>
+      <div>
+        Sing up now to unlock your dictionaries
+      </div>
+      <button
+        className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
+        onClick={() => setShowForm(true)}
+      >
+        Sign up
+      </button>
+      <div>
+            <label for="Email" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Email</label>
+            <input type="text" id="first_name" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="example@mail" required>
+              </input>
+      </div>
+      <input placeholder = "email" onChange={(e) => setData(e.target.value)} value={data} />
+      <input placeholder = "password" onChange={(e) => setData(e.target.value)} value={data} />
+      <input placeholder = "password" onChange={(e) => setData(e.target.value)} value={data} />
+      <div>Alrady have account?</div>
+      <a>Sigin in</a>
+      <input placeholder = "email" onChange={(e) => setData(e.target.value)} value={data} />
+      <input placeholder = "password" onChange={(e) => setData(e.target.value)} value={data} />
+      <div>
+        {data && <div>foo</div>}
+      </div>
+      <input onChange={(e) => setData(e.target.value)} value={data} />
+    </div>
+  )
+}
 
 function IndexPopup() {
   const [data, setData] = useState("")
 
+  const storage = new SecureStorage()
+
+  storage.setPassword("roosevelt")
+
+  const testdata = storage.get("test")
+
+  const [showForm, setShowForm] = useState(false);
+  const [logined, setLogined] = useState(false);
+
   return (
-    <div
-      style={{
-        display: "flex",
-        flexDirection: "column",
-        padding: 16
-      }}>
-      <h2>
-        Welcome to your
-        <a href="https://www.plasmo.com" target="_blank">
-          {" "}
-          Plasmo
-        </a>{" "}
-        Extension!
-      </h2>
-      <input onChange={(e) => setData(e.target.value)} value={data} />
-      <a href="https://docs.plasmo.com" target="_blank">
-        View Docs
-      </a>
+    <div class= "w-80 font-sans p-4">
+      <h1 class = "text-lg font-semibold text-slate-900 border-b">Web-lingo</h1>
+      {logined ? <div>foo</div> : loginComponent({showForm, setShowForm, data, setData})}
     </div>
   )
 }
